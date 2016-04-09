fa10ac1601d4f9f4b3b17d6b9832fe2c26e22075
Cameron Jones <cameroncjones4@gmail.com>
2016-04-09 15:44:13 -0700

    Cleaned up code

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 var cheerio = require('cheerio');
 var Crawler = require("simplecrawler");
 
-//var initialTopic = "Tesla_Motors";
 var initialTopic = "SpaceX";
-var blacklist = ["File:", "#", "/w/", "/static/", "Wikipedia:", "Template:", "MediaWiki:", "Help:", "Special:", "Category:", "Portal:", "Main_Page", "Talk:", "User:", "User_talk:", "Template_talk:", "Module:", "/api/", "/beacon/"]
+var blacklist = ["#", "/w/", "/static/", "/api/", "/beacon/", "File:",
+                 "Wikipedia:", "Template:", "MediaWiki:", "Help:", "Special:",
+                 "Category:", "Portal:", "Main_Page", "Talk:", "User:",
+                 "User_talk:", "Template_talk:", "Module:"]
 
-var crawler = new Crawler("en.wikipedia.org", "/wiki/" + initialTopic);
+var crawler = new Crawler("en.wikipedia.org", "/wiki/" + initialTopic, 80);
+
 crawler.initialProtocol = "https";
-crawler.maxDepth = 3;
+crawler.maxDepth = 2;
 crawler.scanSubdomains = false;
 crawler.stripWWWDomain = true;
 crawler.stripQuerystring = true;
 
 crawler.discoverResources = function(buffer, queue) {
   var $ = cheerio.load(buffer.toString("utf8"));
+  $('.div-col.columns.column-count.column-count-2').prev().nextAll().remove();
   return $('a[href]').map(function() {
     var link = $(this).attr('href');
     if (!new RegExp(blacklist.join("|")).test(link)) {
@@ -23,16 +27,20 @@ crawler.discoverResources = function(buffer, queue) {
 };
 
 crawler.on("fetchcomplete", function(queue, data, res) {
-    var $ = cheerio.load(data);
     var fetchedAddress = queue.url;
+    var $ = cheerio.load(data);
+    $('.div-col.columns.column-count.column-count-2').nextAll().remove();
     var cleanedData = $('#mw-content-text').text()
     console.log(fetchedAddress);
-    console.log("\n");
-    console.log(cleanedData);
-    console.log("-------------------------");
-
-    //console.log("I just received %s (%d bytes)", queue.url, data.length);
-    //console.log("It was a resource of type %s", res.headers['content-type']);
+    //console.log(cleanedData);
 });
 
 crawler.start();
+
+crawler.on("crawlstart", function() {
+  console.log("begin!");
+});
+
+crawler.on("complete", function() { //this event does not fire and the console hangs
+  console.log("end!");
+});
